55c86ba3a8a06593ce25aac59b78ec2536116e82
Gabriel Abubakar <gabrielogrima@gmail.com>
2021-12-04 23:19:41 +0100

    navbar and its responsive config

---
+++ components/navBar/navBar.tsx
@@ -0,0 +1,56 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import Image from 'next/image'
+import styles from './navBar.module.scss'
+
+
+const Navigation: NextPage = () => {
+
+    return (
+        <nav className={styles.navigationBar}>
+
+            <Image src="/images/shared/logo.svg" width={48} height={48} alt="main logo"/>
+
+            <ul className={styles.navigation}>
+                <li>
+                    <Link href="/">
+                        <a>
+                            <span>00</span>
+                            Home
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <a>
+                            <span>01</span>
+                            Destination
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <a>
+                            <span>02</span>
+                            Crew
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/">
+                        <a>
+                            <span>03</span>
+                            Technology
+                        </a>
+                    </Link>
+                </li>
+            </ul>
+
+            {/* <div className={styles.go} >
+                <Image src="/images/shared/icon-hamburger.svg" height={24} width={21} className={styles.god}/>
+            </div> */}
+        </nav>
+    )
+}
+
+export default Navigation;
